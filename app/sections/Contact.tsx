03ae32301a41deb993861a2487a3da940ee4c916
Sugeng Permana Desembry <mylistensong6@gmail.com>
2026-02-13 15:25:34 +0700

    update

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -4,12 +4,13 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="relative overflow-hidden pb-10 border-t border-white/10 mt-40 w-full"
+      className="relative overflow-hidden pb-10 border-t border-white/10 mt-40 w-full "
     >
-      <div className="max-w-6xl mx-auto px-6 py-20 relative z-10">
-        <div className="transition-all duration-1000 ease-out transform opacity-100 translate-y-0 text-center">
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-px bg-linear-to-r from-transparent via-white/10 to-transparent"></div>
+      <div className="max-w-6xl mx-auto px-6 py-12 relative z-10">
+        <div className="text-center">
           <ShinyText
-            className="inline-flex items-center gap-2 px-4 py-1.5 rounded-full bg-linear-to-r from-purple-500/10 to-blue-500/10 border border-primary backdrop-blur-sm mb-8 group hover:scale-105 transition-transform duration-300"
+            className="inline-flex items-center gap-2 px-4 py-1.5 rounded-full bg-linear-to-r from-purple-500/10 to-blue-500/10 border border-primary backdrop-blur-sm mb-15 group hover:scale-105 transition-transform duration-300"
             text="✨ Get In Touch "
             speed={2}
             delay={1}
@@ -21,7 +22,9 @@ const Contact = () => {
             pauseOnHover={false}
             disabled={false}
           />
+        </div>
 
+        <div className="transition-all duration-1000 ease-out transform opacity-100 translate-y-0 ">
           <div className="flex flex-col gap-10">
             <div className="flex gap-6">
               <Link
@@ -36,7 +39,7 @@ const Contact = () => {
                   stroke-width="2"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  className="h-16 w-16"
+                  className="h-15 w-15"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4l0 -8" />
@@ -53,10 +56,10 @@ const Contact = () => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="h-16 w-16"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="h-15 w-15"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
@@ -75,14 +78,14 @@ const Contact = () => {
                   stroke-width="2"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  className="h-16 w-16"
+                  className="h-15 w-15"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                 </svg>
               </Link>
             </div>
-            <div className="max-w-4xl mt-4">
+            <div className="max-w-3xl mt-4">
               <h2 className="text-3xl md:text-5xl font-bold text-white leading-tight">
                 Want to know more about me, tell me about your project or just
                 to say hello?
@@ -91,6 +94,7 @@ const Contact = () => {
               </h2>
             </div>
           </div>
+          <div className="w-full h-[1px] bg-white/10 my-4"></div>
         </div>
       </div>
     </section>
